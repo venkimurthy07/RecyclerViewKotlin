4b1fb6d5a93c6629c842808f9c34b0113a3dbb20
venkateshmurthy <venkatesh.murthy@compassitesinc.com>
2017-11-27 19:02:07 +0530

    Completed json reading and loading in to recyclerview

--- app/src/main/java/com/venkatesh/recyclerviewkotlin/RecyclerMainActivity.kt
+++ app/src/main/java/com/venkatesh/recyclerviewkotlin/RecyclerMainActivity.kt
@@ -19,23 +19,11 @@ import java.sql.Wrapper
  */
 class RecyclerMainActivity : AppCompatActivity() {
 
-    inner class Wrapper {
-        var version = 1
-        var content: Any? = null
-    }
-    object StaticFields {
-        @JvmStatic
-        val name: String = "RecyclerMainActivity"
-    }
 
-    val listCategory: MutableList<CategoryModel> = ArrayList<CategoryModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        loadCategoryFromJson()
-
 
         init()
 
@@ -49,16 +37,10 @@ class RecyclerMainActivity : AppCompatActivity() {
         val read = loadJSONFromAsset()
         if(read != null)
         {
-
             val gson = Gson()
             val json = gson.toJson(read)
             val `object` = gson.fromJson(json, Any::class.java)
-
-//            val w = Wrapper()
-//            w.content = `object`
-
             val topic = gson.fromJson(`object`.toString(), Array<CategoryModel>::class.java)
-          //  Log.e(StaticFields.name, "json="+topic.size)
             recyclerView.adapter = CategoryAdapter(topic)
 
         }
@@ -79,7 +61,6 @@ class RecyclerMainActivity : AppCompatActivity() {
 
             `is`.close()
 
-            //json = String(buffer, "UTF-8")
 
            json = String(buffer, Charset.forName("UTF-8"))
 
@@ -94,10 +75,5 @@ class RecyclerMainActivity : AppCompatActivity() {
     }
 
 
-    fun loadCategoryFromJson() {
-        val cat = CategoryModel("11", "Fruits", "Apple", "22")
-        listCategory.add(cat)
-        // print(listCategory)
-        Log.e(StaticFields.name, listCategory.toString())
-    }
+
 }
